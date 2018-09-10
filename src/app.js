b51b599b3772cfd40b29e88c68a64456b85d6637
Christophe Johanny <c.johanny@gmail.com>
2018-09-10 11:35:27 +0200

    Remove exemple expense data

--- src/app.js
+++ src/app.js
@@ -13,14 +13,6 @@ import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 12000, createdAt: 1536258603754  }))
-store.dispatch(addExpense({ description: 'Gas bill', amount: 15000, createdAt: 1536258623754 }))
-store.dispatch(addExpense({ description: 'rent', amount: 198000, createdAt: 1536258613754  }))
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
