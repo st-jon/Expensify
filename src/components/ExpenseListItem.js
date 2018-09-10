85598cd97e4079a9449e677ef6cd99ea2d11fa09
Christophe Johanny <c.johanny@gmail.com>
2018-09-10 15:03:45 +0200

    setup formatting amount and createdAT

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import moment from 'moment'
 
 export const ExpenseListItem = ({ id, description, amount, createdAt}) => ( 
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} // {createdAt}</p>
+            <p>
+            {new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(amount / 100)}
+            // 
+            {moment(createdAt).locale('fr').format('LL')}
+            </p>
     </div>
 )
 
